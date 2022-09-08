434c31c51d3ad737442a31b38ccb6dbde0da963b
Вячеслав Форофонов <DanielResult1@outlook.com>
2022-09-08 21:14:58 +0300

    Добавьте файлы проекта.

---
+++ Homework_24/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = {9, 1, 6, 2, 5, 8, 17, 18, 28, 21};
+        int arraySort;
+
+        foreach (int i in array)
+        {
+            Console.Write($" {i}");
+        }
+        for (int i = 0; i < array.Length; i++)
+        {
+            for (int j = 0; j < array.Length; j++)
+            {
+                if (array[i] < array[j])
+                {
+                    arraySort = array[i];
+                    array[i] = array[j];
+                    array[j] = arraySort;
+                }
+            }
+        }
+        Console.Write(" | ");
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.Write($" {array[i]}");
+        }
+    }
+}
